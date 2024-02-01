c3fcef03ca1ff8e44fbe27be9047089d1abc1982
Matt Roumaya <matt.roumaya@atorusresearch.com>
2024-02-01 11:29:27 +0000

    add note for adding to suggests

--- R/init_test_cases.R
+++ R/init_test_cases.R
@@ -10,26 +10,25 @@ init_test_cases <- function() {
 
   if (check_project_status()) {
 
+    # determine the qualification testing path
     directory_path <- paste0(
       usethis::proj_path(),
       "/tests/testqualification/qualification"
       )
 
+    # create the directory above
     create_directory(directory_path = directory_path)
 
+    # copy from `~inst/...` - sample qualification test case
     copy_test_case_template(directory_path = directory_path)
 
-
+    # use the {testthat} library - now need to add as a Suggests dependency
+    usethis::ui_done("Adding [ {testthat} ] to Suggests")
     use_dep <- utils::getFromNamespace("use_dependency", "usethis")
-
     use_dep("testthat", "Suggests")
-
   } else {
     usethis::ui_oops("You are either not in an R project or your working directory has been changed.")
   }
-
-
-
 }
 
 

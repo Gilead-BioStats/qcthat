1bfcb9ec18afff958e82e7499360f24f5e8e1e8e
Matt Roumaya <matt.roumaya@atorusresearch.com>
2024-02-01 18:51:15 +0000

    update initialize function

--- R/init_github_actions.R
+++ R/init_github_actions.R
@@ -6,11 +6,9 @@
 init_github_actions <- function() {
 
   if (check_project_status()) {
-
     usethis::use_github_action(
       url = "https://raw.githubusercontent.com/Gilead-BioStats/qcthat/updates/inst/template/qualification_report.yaml?token=GHSAT0AAAAAAB73423PXSOE5F44XAMADPAAZN3X6DQ"
       )
   }
-
 
 }

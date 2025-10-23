732e6cb02c2ab32ee0b34737111b12e8b8831ad7
Jon Harmon <jon.harmon@atorusresearch.com>
2025-10-23 10:48:07 -0500

    Fix formatting in print help.

--- R/PrintTestMatrix.R
+++ R/PrintTestMatrix.R
@@ -16,8 +16,10 @@
 #' @examplesIf interactive()
 #'
 #'   lTestResults <- testthat::test_local(stop_on_failure = FALSE)
-#'   IssueTestMatrix <- CompileIssueTestMatrix( dfRepoIssues =
-#'   FetchRepoIssues(), dfTestResults = CompileTestResults(lTestResults) )
+#'   IssueTestMatrix <- CompileIssueTestMatrix(
+#'     dfRepoIssues = FetchRepoIssues(),
+#'     dfTestResults = CompileTestResults(lTestResults)
+#'   )
 #'   print(IssueTestMatrix)
 #'
 #'   # Remove the "qcthat_IssueTestMatrix" class to see the raw tibble.

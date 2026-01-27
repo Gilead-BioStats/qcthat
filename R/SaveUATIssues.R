728f36f1c597af0b8e432a414672b74017df0fc1
Jon Harmon <jon.harmon@atorusresearch.com>
2026-01-27 13:06:37 -0600

    Add `SaveUATIssues()`/`LoadUATIssues()`
    Closes #164

---
+++ R/SaveUATIssues.R
@@ -0,0 +1,26 @@
+#' Save UAT issues to disk
+#'
+#' Save the internal registry of user acceptance testing (UAT) issues to an RDS
+#' file. This allows the UAT state to be persisted across R sessions.
+#'
+#' @param strPath (`length-1 character`) Path to the UAT file. Defaults to
+#'   `"UATIssues.rds"` in the working directory.
+#'
+#' @returns `NULL` invisibly.
+#' @export
+SaveUATIssues <- function(strPath = "UATIssues.rds") {
+  saveRDS(envQcthat$UATIssues, strPath)
+}
+
+#' Load UAT issues from disk
+#'
+#' Load a registry of user acceptance testing (UAT) issues from an RDS file into
+#' the internal package environment.
+#'
+#' @inheritParams SaveUATIssues
+#'
+#' @returns The UAT issues data frame, invisibly.
+#' @export
+LoadUATIssues <- function(strPath = "UATIssues.rds") {
+  envQcthat$UATIssues <- readRDS(strPath)
+}

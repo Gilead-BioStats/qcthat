8255a6fddbd1e810ed43cfb1d0437493f5a4abbf
Jon Harmon <jon.harmon@atorusresearch.com>
2026-04-01 15:42:39 -0500

    Clean up old UAT wording and remove unused UAT.

--- tests/testthat/test-Actions.R
+++ tests/testthat/test-Actions.R
@@ -38,24 +38,11 @@ test_that("Action_qcthat targets the expected action (#55, #68, #69, #73, #88, #
   expect_identical(Action_qcthat(), "qcthat")
 })
 
-test_that("qcthat is installed as part of the GHA (#95)", {
-  qcthat::ExpectUserAccepts(
-    "The qcthat GHAs install qcthat",
-    intIssue = 95,
-    chrInstructions = "Call the functions in a repo other than qcthat, with qcthat installed locally. Ensure that the installed workflows install qcthat.",
-    chrChecks = c(
-      "The action added to a repository via Action_QCCompletedIssues() installs qcthat.",
-      "The action added to a repository via Action_QCPRIssues() installs qcthat.",
-      "The action added to a repository via Action_QCMilestone() installs qcthat."
-    )
-  )
-})
-
 test_that("Reports generated via GHA include information about the issues (#77, #37)", {
   qcthat::ExpectUserAccepts(
     "qcthat reports show the expected issue information",
     intIssue = 77,
-    chrInstructions = "Check the reports in any PRs attached to issue #77.",
+    chrInstructions = "Check the reports in the PR mentioned at the end of this issue (scroll to the bottom of this page, look for 'github-actions mentioned this').",
     chrChecks = c(
       "Newly generated qcthat reports show the type, title, status, and milestone (if any) of issues."
     )
@@ -69,25 +56,6 @@ test_that("Additional user acceptance sub-issue is generated in the qcthat User 
     chrInstructions = "Check {qcthat} Report: User Acceptance ",
     chrChecks = c(
       "Generated qcthat reports shows additional sub-issue for user acceptance test #83."
-    )
-  )
-})
-
-test_that("qcthat is namespaced in GHA (#143)", {
-  qcthat::ExpectUserAccepts(
-    "The installed qcthat GHAs use namespaced function calls",
-    intIssue = 143,
-    chrInstructions = paste(
-      "1. Install this version of qcthat.",
-      "2. In a repo other than qcthat, call `qcthat::Action_QCPRIssues(TRUE)` (to upgrade if it's already installed).",
-      "3. Open the installed action.",
-      "4. Assign a real PR number from that repo to `prNumber`.",
-      "5. Copy/paste the rest of the code from the 'Generate PR-Associated Issues QC Report' section of the installed action.",
-      "6. Optionally delete the comments in the chosen PR if all of that works.",
-      sep = "\n"
-    ),
-    chrChecks = c(
-      "The code runs without error (other than non-qcthat issues in the tests themselves)."
     )
   )
 })

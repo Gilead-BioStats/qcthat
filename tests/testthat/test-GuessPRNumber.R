084650b5512e3b7e959291e9c8fb0136058bff9f
Jon Harmon <jon.harmon1@gilead.com>
2026-05-21 10:04:53 -0500

    Tag test to issue

--- tests/testthat/test-GuessPRNumber.R
+++ tests/testthat/test-GuessPRNumber.R
@@ -17,7 +17,7 @@ test_that("GetGHAPRNumber returns NULL for bad arg (#84, #163)", {
   expect_null(GetGHAPRNumber(list()))
 })
 
-test_that("GetGHAPRNumber extracts PR number from lGHEventPayload when available (#84, #163)", {
+test_that("GetGHAPRNumber extracts PR number from lGHEventPayload when available (#84, #163, #310)", {
   expect_equal(
     GetGHAPRNumber(list(pull_request = list(number = 42))),
     42
